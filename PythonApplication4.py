af81cff67a90736225b51bd27684a603e8615f09
anwar zigler <anouar2002zigler@gmail.com>
2021-03-06 21:37:44 +0100

    Add project files.

---
+++ PythonApplication4.py
@@ -0,0 +1,13 @@
+#video downloader with python
+from pytube import YouTube;
+
+url = input('video url : ');
+print('downloading..');
+
+yt = YouTube(url);
+
+video = yt.streams.get_by_resolution("720p");
+video.download(output_path='D:/');
+print('downloaded');
+
+#thanks for watching 
